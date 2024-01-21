03d819702b2a514e7be8411cac5f599822c72d2b
ReeyaDono <metrytmc@gmail.com>
2024-01-21 17:04:21 +0700

    Add stalonetray

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -11,8 +11,10 @@ dunst &
 
 # Terminate already running bar instances
 killall -q polybar
+# killall -q stalonetray
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
+# stalonetray & sleep 0.1 && xdo hide -N stalonetray && touch "/tmp/syshide.lock"
 polybar -r bar-left 2>&1 | tee -a /tmp/polybar.log & disown
 polybar -r bar-right 2>&1 | tee -a /tmp/polybar.log & disown
 polybar -r bar-center 2>&1 | tee -a /tmp/polybar.log & disown
